9c536a0a07dc237c6806a9247d5c025e164fa801
cssemister5-beep <cssemister5@gmail.com>
2025-09-24 19:27:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -342,5 +342,10 @@ def proxy(url):
     except Exception as e:
         return f"Proxy error: {e}", 500
 
-if __name__ == "__main__":
-    app.run(debug=True)
+import os
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))  # fallback to 10000 locally
+    app.run(host='0.0.0.0', port=port)
